25f3b6286ebfe612256900b31270427942c10a73
longfengpili <398745129@qq.com>
2019-07-04 11:24:00 +0800

    psetting

--- psetting.py
+++ psetting.py
@@ -2,7 +2,7 @@
 '''
 @Author: longfengpili
 @Date: 2019-06-19 15:34:00
-@LastEditTime: 2019-07-03 18:14:48
+@LastEditTime: 2019-07-03 15:51:59
 @coding: 
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
@@ -44,23 +44,21 @@ R_DATABASE = mysetting.redshift_database
 R_HOST = mysetting.redshift_host
 R_USER = mysetting.redshift_user
 R_PASSWORD = mysetting.redshift_password
-## parse
 R_REPAIR_TABLENAME = mysetting.r_repair_table
 R_RESOLVE_TABLENAME = mysetting.r_resolve_table
 R_RESOLVE_COLUMNS = mysetting.resolve_columns
-## daily
-EXECUTE_ORDER = mysetting.execute_order
 
 # Mysql
 M_DATABASE = mysetting.mysql_database
-M_HOST = mysetting.mysql_j_host
+M_HOST = mysetting.mysql_host
 M_USER = mysetting.mysql_user
-M_PASSWORD = mysetting.mysql_j_password
+M_PASSWORD = mysetting.mysql_password
 M_ORIGINAL_TABLENAME = mysetting.original_table
 M_ORIGINAL_COLUMNS = mysetting.original_columns
 M_REPAIR_TABLENAME = mysetting.repair_table
 M_RESOLVE_TABLENAME = mysetting.resolve_table
 M_RESOLVE_COLUMNS = mysetting.resolve_columns
-
+# daily
+EXECUTE_ORDER = mysetting.execute_order
 
 

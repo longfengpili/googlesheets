eac55581e92dab85ff1e76b484d00c97450f6d9f
longfengpili <398745129@qq.com>
2019-07-04 16:19:55 +0800

    job setting

--- daily_execute_sql.py
+++ daily_execute_sql.py
@@ -1,24 +1,20 @@
 '''
 @Author: longfengpili
 @Date: 2019-07-01 14:11:55
-@LastEditTime: 2019-07-03 18:51:48
+@LastEditTime: 2019-07-04 16:16:54
 @coding: 
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 @github: https://github.com/longfengpili
 '''
+import sys
 from daily_work import DailyMainRedshift
 from psetting import *
 from datetime import datetime, date, timedelta
 
 now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-import sys
-params = sys.argv
-if 2 <= len(params) < 4:
-    for i in range(4 - len(params)):
-        params.append('0')
-params_execute = ' '.join(params[1:])
+
 
 def set_date(interval_day):
     today = date.today()
@@ -28,19 +24,22 @@ def set_date(interval_day):
 
 def daily_work_main(date_min, date_max, now, **kw):
     dm = DailyMainRedshift(host=R_HOST, user=R_USER,
-                           password=R_PASSWORD, database=R_DATABASE)
+                           password=R_PASSWORD, database=R_DATABASE, sqlpath=SQL_PATH)
     dm.daily_execute_all(execute_order=EXECUTE_ORDER, date_min=date_min, date_max=date_max, now=now, **kw)
 
 
 def daily_work_single_main(schema, date_min, date_max, now, **kw):
     dm = DailyMainRedshift(host=R_HOST, user=R_USER,
-                           password=R_PASSWORD, database=R_DATABASE)
+                           password=R_PASSWORD, database=R_DATABASE, sqlpath=SQL_PATH)
     dm.daily_execute_single(schema=schema, date_min=date_min, date_max=date_max, now=now, **kw)
 
 
+params = sys.argv
+params = params[1:]
+
 # 格式："\033[字背景颜色；字体颜色m————————\033[0m"   (——————表示字符串)
-if not params_execute:
-    params_execute = input(f'''every params please add blank !
+if not params:
+    params = input(f'''every params please add blank !
 【PARAM_1】which sqlfile?
     【all】     ：all
     【raw】     ：raw_data
@@ -52,7 +51,13 @@ if not params_execute:
 【PARAM_3】to end days?
     example:today is 0, yesterday is -1
 请选择要执行的内容：''')
+    params = params.split(' ')
 
+if 1 <= len(params) < 3:
+    for i in range(3 - len(params)):
+        params.append('0')
+
+params_execute = ' '.join(params)
 if not params_execute:
     p1, p2, p3 = 'all', '0', '0'
 else:

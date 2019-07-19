0cbf2107dbba67205806494a116ae7670c494a69
longfengpili <398745129@qq.com>
2019-07-19 14:38:35 +0800

    reset auto increment id

--- db_api/db_api.py
+++ db_api/db_api.py
@@ -1,7 +1,7 @@
 '''
 @Author: longfengpili
 @Date: 2019-06-20 12:37:41
-@LastEditTime: 2019-07-19 13:22:07
+@LastEditTime: 2019-07-19 14:37:12
 @coding: 
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-

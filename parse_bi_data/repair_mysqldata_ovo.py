2b0d3d59cb3da809ac0bfef7bdf9d2f272f7be96
longfengpili <398745129@qq.com>
2019-08-15 09:53:42 +0800

    error

--- parse_bi_data/repair_mysqldata_ovo.py
+++ parse_bi_data/repair_mysqldata_ovo.py
@@ -1,7 +1,7 @@
 '''
 @Author: longfengpili
 @Date: 2019-06-27 14:41:34
-@LastEditTime: 2019-08-14 16:11:44
+@LastEditTime: 2019-08-15 09:53:25
 @coding: 
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
@@ -134,6 +134,7 @@ class RepairMysqlDataOVO(ParseBiFunc):
             myjson = json.dumps(myjson)
 
             error = '\n'.join(rjd.errors)
+            error = error.replace('【error】',f'【error】【{id}】')
             repairbi_logger.error(f"{error}")
         return id, myjson, rjd.errors
 

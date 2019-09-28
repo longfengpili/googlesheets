ac142bd930243814e9e96d32491c68b4607fcfa2
longfengpili <398745129@qq.com>
2019-09-28 16:10:29 +0800

    load_country_from_sdk.py

---
+++ load_country_from_sdk.py
@@ -0,0 +1,25 @@
+'''
+@Author: longfengpili
+@Date: 2019-07-10 20:15:58
+@LastEditTime: 2019-09-28 14:27:57
+@github: https://github.com/longfengpili
+'''
+
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+from db_api import DBRedshift
+from psetting import *
+
+
+db1 = DBRedshift(host=SDK_REDSHIFT_HOST, user=SDK_REDSHIFT_USER, password=SDK_REDSHIFT_PASSWORD, database=SDK_REDSHIFT_DATABASE)
+sql = 'select distinct fpid, country from animatch_bi.event_kpi_login;'
+count, result = db1.sql_execute(sql)
+
+db2 = DBRedshift(host=R_HOST, user=R_USER, password=R_PASSWORD, database=R_DATABASE)
+tablename = 'fact_data_aniland.sdk_country'
+columns={'fpid': 'varchar', 'country':'varchar'}
+db2.drop_table(tablename)
+db2.create_table(tablename, columns=columns)
+sql = db2.sql_for_insert(tablename, columns, result)
+db2.sql_execute(sql)

75aaf14b22b9cc449a74d6847f036ba3a2a31afe
longfengpili <398745129@qq.com>
2019-10-02 10:49:33 +0800

    sql

--- load_country_from_sdk.py
+++ load_country_from_sdk.py
@@ -13,7 +13,12 @@ from psetting import *
 
 
 db1 = DBRedshift(host=SDK_REDSHIFT_HOST, user=SDK_REDSHIFT_USER, password=SDK_REDSHIFT_PASSWORD, database=SDK_REDSHIFT_DATABASE)
-sql = 'select distinct fpid, country from animatch_bi.d_adjust;'
+sql = '''select distinct fpid,
+upper(first_value(country ignore nulls) over (partition by fpid order by created_at
+rows between unbounded preceding and unbounded following)) as country
+from animatch_bi.event_adjust_callback
+where fpid is not null
+;'''
 count, result = db1.sql_execute(sql)
 
 db2 = DBRedshift(host=R_HOST, user=R_USER, password=R_PASSWORD, database=R_DATABASE)

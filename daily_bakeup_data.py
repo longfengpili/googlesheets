2400324e8b55a10f91c47980c5bec310a7e56133
longfengpili <398745129@qq.com>
2019-10-11 20:44:22 +0800

    [减少]注释掉部分功能

--- daily_bakeup_data.py
+++ daily_bakeup_data.py
@@ -1,7 +1,7 @@
 '''
 @Author: longfengpili
 @Date: 2019-07-10 20:15:58
-@LastEditTime: 2019-10-11 20:32:03
+@LastEditTime: 2019-10-11 20:43:22
 @github: https://github.com/longfengpili
 '''
 
@@ -29,7 +29,7 @@ rmdovo = CopyDataOVO(db_type='mysql', db_host=M_HOST, db_user=M_USER, db_passwor
                             original_columns=M_BUKEUP_GAME_COLUMNS)
 rmdovo.copy_game_data_main(original_tablename=M_BUKEUP_GAME_TABLENAME,repair_tablename=NEW_M_BUKEUP_GAME_TABLENAME, id_min=id_min, id_max=id_max, is_repair=False)
 
-rmdovo = CopyDataOVO(db_type='mysql', db_host=M_HOST, db_user=M_USER, db_password=M_PASSWORD, db_database=M_DATABASE,
-                            db2_type='mysql', db2_host=M_BUKEUP_HOST, db2_user=M_BUKEUP_USER, db2_password=M_BUKEUP_PASSWORD, db2_database=M_BUKEUP_DATABASE,
-                            original_columns=M_BUKEUP_ADJUST_COLUMNS)
-rmdovo.copy_game_data_main(original_tablename=M_BUKEUP_GAME_TABLENAME,repair_tablename=NEW_M_BUKEUP_ADJUST_TABLENAME, id_min=id_min, id_max=id_max, is_repair=False)                       
+# rmdovo = CopyDataOVO(db_type='mysql', db_host=M_HOST, db_user=M_USER, db_password=M_PASSWORD, db_database=M_DATABASE,
+#                             db2_type='mysql', db2_host=M_BUKEUP_HOST, db2_user=M_BUKEUP_USER, db2_password=M_BUKEUP_PASSWORD, db2_database=M_BUKEUP_DATABASE,
+#                             original_columns=M_BUKEUP_ADJUST_COLUMNS)
+# rmdovo.copy_game_data_main(original_tablename=M_BUKEUP_GAME_TABLENAME,repair_tablename=NEW_M_BUKEUP_ADJUST_TABLENAME, id_min=id_min, id_max=id_max, is_repair=False)                       

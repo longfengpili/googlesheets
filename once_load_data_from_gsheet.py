2c2465da55220b504c0424d6c9ba5e2dcdd693dd
longfengpili <398745129@qq.com>
2019-10-11 10:13:33 +0800

    quest

--- once_load_data_from_gsheet.py
+++ once_load_data_from_gsheet.py
@@ -1,7 +1,7 @@
 '''
 @Author: longfengpili
 @Date: 2019-07-10 20:15:58
-@LastEditTime: 2019-09-26 11:57:42
+@LastEditTime: 2019-10-09 11:10:16
 @github: https://github.com/longfengpili
 '''
 
@@ -25,6 +25,7 @@ please choice reload data !!!!
 1. reload item info
 2. reload bi table
 3. reload new user funnel (0926)
+4. reload quest (0926)
 
 you want run :''')
 
@@ -34,5 +35,7 @@ elif load == '2':
     save_main(spreadsheet_id=BI_SPREADSHEET_ID, sheetname=BI_SHEETNAME, tablename=R_BI_TABLENAME, columns=BI_COLUMNS, primary_key=False)
 elif load == '3':
     save_main(spreadsheet_id=FUNNEL_SPREADSHEET_ID, sheetname=FUNNEL_SHEETNAME, tablename=R_FUNNEL_TABLENAME, columns=FUNNEL_COLUMNS, primary_key=True)
+elif load == '4':
+    save_main(spreadsheet_id=QUEST_SPREADSHEET_ID, sheetname=QUEST_SHEETNAME, tablename=R_QUEST_TABLENAME, columns=QUEST_COLUMNS, primary_key=True)
 
 
